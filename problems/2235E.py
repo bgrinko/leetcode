316e67e26a2fb3be2443391045cc712b5d20c290
basil.grinko <basil.grinko@gmail.com>
2022-06-26 18:47:46 +0300

    2235. Add Two Integers

---
+++ problems/2235E.py
@@ -0,0 +1,11 @@
+"""LeetCode problem 2235. Add Two Integers
+
+Link:       https://leetcode.com/problems/add-two-integers/
+Difficulty: Easy
+Status:     Accepted (06/26/2022 18:47) 35 ms 13.8 MB
+"""
+
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1 + num2
