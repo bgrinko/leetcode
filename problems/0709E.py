e6a1bea88a2532c7fa6eb13eaf79d8a03ad490f7
basil.grinko <basil.grinko@gmail.com>
2022-06-27 02:40:54 +0300

    709. To Lower Case

---
+++ problems/0709E.py
@@ -0,0 +1,11 @@
+"""LeetCode problem 709. To Lower Case
+
+Link:       https://leetcode.com/problems/to-lower-case/
+Difficulty: Easy
+Status:     Accepted (06/27/2022 02:40) 49 ms 13.8 MB
+"""
+
+
+class Solution:
+    def toLowerCase(self, s: str) -> str:
+        return s.lower()
