7640bf76efc7e5b5eb943b83e95dd5de4f39e732
basil.grinko <basil.grinko@gmail.com>
2022-06-26 18:50:11 +0300

    1929. Concatenation of Array

---
+++ problems/1929E.py
@@ -0,0 +1,11 @@
+"""LeetCode problem 1929. Concatenation of Array
+
+Link:       https://leetcode.com/problems/concatenation-of-array/
+Difficulty: Easy
+Status:     Accepted (06/26/2022 18:49) 138 ms 14.2 MB
+"""
+
+
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        return nums + nums
