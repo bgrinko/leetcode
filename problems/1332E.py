554c1dd1f3858b89c5ec90636a65f3b68ce764ab
basil.grinko <basil.grinko@gmail.com>
2022-06-30 21:50:04 +0300

    1332. Remove Palindromic Subsequences

--- problems/1332E.py
+++ problems/1332E.py
@@ -7,7 +7,5 @@ Status:     Accepted (06/30/2022 21:49) 52 ms 13.9 MB
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        for i in range(n):
-            nums1[i + m] = nums2[i]
-        nums1.sort()
+    def removePalindromeSub(self, s: str) -> int:
+        return 1 if s == s[::-1] else 2
