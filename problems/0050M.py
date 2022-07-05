da78b1bb894e91a5423408f1ffc193cb1df7c6b6
basil.grinko <basil.grinko@gmail.com>
2022-07-05 21:58:18 +0300

    50. Pow(x, n)

---
+++ problems/0050M.py
@@ -0,0 +1,11 @@
+"""LeetCode problem 50. Pow(x, n)
+
+Link:       https://leetcode.com/problems/powx-n/
+Difficulty: Medium
+Status:     Accepted (07/05/2022 21:57) 44 ms 13.8 MB
+"""
+
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        return x**n
