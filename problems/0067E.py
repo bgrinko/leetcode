bea189ab8ba86315873b683d09a9a482433e8329
basil.grinko <basil.grinko@gmail.com>
2022-07-06 00:10:35 +0300

    67. Add Binary

---
+++ problems/0067E.py
@@ -0,0 +1,11 @@
+"""LeetCode problem 67. Add Binary
+
+Link:       https://leetcode.com/problems/add-binary/
+Difficulty: Easy
+Status:     Accepted (07/06/2022 00:09) 62 ms 13.8 MB
+"""
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return str(bin(int(a, 2)+int(b, 2))).replace("0b", "")
